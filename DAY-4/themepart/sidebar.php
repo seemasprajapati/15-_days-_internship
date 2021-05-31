4e3e85aa6efdd6a6b79c25ec5dc6ad0d45b17ee1
Seema Prajapati <84803005+seemasprajapati@users.noreply.github.com>
2021-05-31 22:15:23 +0530

    Create sidebar.php

---
+++ DAY-4/themepart/sidebar.php
@@ -0,0 +1,14 @@
+<div id="sidebar">
+			<ul>
+					<h2>Our Work</h2>
+					<ul>
+						<li><a href="web.php">web development</a></li>
+						<li><a href="mobile.php">mobile app development</a></li>
+						
+					</ul>
+				</li>
+			</ul>
+           <marquee>
+               welcome to my website.
+               </marquee>
+		</div>
